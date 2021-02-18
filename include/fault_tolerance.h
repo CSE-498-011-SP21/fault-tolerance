a8421246ab18df8389ef4f7a54cc35935ad89929
Cody D'Ambrosio <61693810+cjd218@users.noreply.github.com>
2021-02-18 15:04:46 -0500

    Add files via upload

--- include/fault_tolerance.h
+++ include/fault_tolerance.h
@@ -7,7 +7,33 @@
 #ifndef FAULT_TOLERANCE_H
 #define FAULT_TOLERANCE_H
 
+/**
+ * @file
+ *
+ * @brief Public API for KVCG Fault Tolerance protocol
+ *
+ */
+
+/**
+ *
+ * Initialize server
+ *
+ * @param None
+ *
+ * @return integer
+ *
+ */
 int init_server();
+
+/**
+ *
+ * Initialize client
+ *
+ * @param None
+ *
+ * @return integer
+ *
+ */
 int init_client();
 
 #endif // FAULT_TOLERANCE_H
