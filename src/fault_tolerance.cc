f2d909b30edb89af5768841b63c497f7941dd7b9
Cody D'Ambrosio <cjd218@lehigh.edu>
2021-02-22 15:08:46 -0500

    Add basic JSON parsing
    Add basic JSON parsing and sample file.
    
    Signed-off-by: Cody D'Ambrosio <cjd218@lehigh.edu>

--- src/fault_tolerance.cc
+++ src/fault_tolerance.cc
@@ -8,6 +8,7 @@
 #include <sstream>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/foreach.hpp>
 
 namespace pt = boost::property_tree;
 
@@ -18,18 +19,33 @@ int LOG_LEVEL = INFO;
 
 int parse_json_file() {
     int status = 0;
+    int backupcnt = 0;
     LOG(DEBUG) << "Opening file: " << CFG_FILE;
 
     pt::ptree root;
     try {
         pt::read_json(CFG_FILE, root);
-    } catch (const boost::property_tree::json_parser_error &e) {
-        LOG(ERROR) << "Failed reading " << CFG_FILE << ": " << e.message().c_str();
-        return 1;
+        for (pt::ptree::value_type &server : root.get_child("servers")) {
+            LOG(DEBUG) << server.second.get<std::string>("name");
+            backupcnt = 0;
+            BOOST_FOREACH(pt::ptree::value_type &backup, server.second.get_child("backups")) {
+                LOG(DEBUG) << "  backup: " << backup.second.data();
+                backupcnt++;
+            }
+            if (backupcnt != 1) {
+                // TBD: For now only support exactly 1 backup
+                LOG(ERROR) << "Only supporting 1 backup";
+                status = 1;
+                goto exit;
+            }
+        }
+    } catch (std::exception const& e) {
+        LOG(ERROR) << "Failed reading " << CFG_FILE << ": " << e.what();
+        status = 1;
+        goto exit;
     }
-    // get fields with...
-    // int threads = root.get<int>("threads", DEFAULT_NUM_THREADS);
 
+exit:
     return status;
 }
 
@@ -37,10 +53,10 @@ int Server::initialize() {
     int status = 0;
     LOG(INFO) << "Initializing Server";
 
-    if (status = parse_json_file()) {
-        return status;
-    }
+    if (status = parse_json_file())
+        goto exit;
 
+exit:
     return status;
 }
 
@@ -48,9 +64,9 @@ int Client::initialize() {
     int status = 0;
     LOG(INFO) << "Initializing Client";
 
-    if (status = parse_json_file()) {
-      return status;
-    }
+    if (status = parse_json_file())
+        goto exit;
 
+exit:
     return status;
 }
