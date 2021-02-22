3540ceef98e7f2bf329351f923f03e0a35b86c65
Cody D'Ambrosio <cjd218@lehigh.edu>
2021-02-22 11:41:13 -0500

    Improve help text
    Add explicit -h argument for help
    
    Signed-off-by: Cody D'Ambrosio <cjd218@lehigh.edu>

--- test/unittest_fault_tolerance.cc
+++ test/unittest_fault_tolerance.cc
@@ -10,9 +10,10 @@
 
 void usage() {
   std::cout << "Usage: unittest_fault_tolerance [OPTIONS]" << std::endl;
-  std::cout << "  -c [CONFIG] : Config JSON file (Default:" << CFG_FILE << ")" << std::endl; 
-  std::cout << "  -C          : Run as client" << std::endl;
+  std::cout << "  -c [CONFIG] : Config JSON file (Default: " << CFG_FILE << ")" << std::endl; 
+  std::cout << "  -C          : Run as client, defaults to running as server " << std::endl;
   std::cout << "  -v          : Increase verbosity" << std::endl;
+  std::cout << "  -h          : Print this help text" << std::endl;
   std::cout << std::endl;
 }
 int main(int argc, char* argv[]) {
@@ -20,17 +21,13 @@ int main(int argc, char* argv[]) {
     int opt;
     bool client = false;
 
-    while ((opt = getopt(argc, argv, "c:Cv")) != -1) {
+    while ((opt = getopt(argc, argv, "c:Cvh")) != -1) {
       switch(opt) {
         case 'c': CFG_FILE = optarg; break;
         case 'C': client = true; break;
         case 'v': LOG_LEVEL++; break;
-        case '?':
-            usage();
-            return 1;
-        default:
-            usage();
-            return 1;
+        case 'h': usage(); return 0; break;
+        default:  usage(); return 1; break;
       }
     }
 
