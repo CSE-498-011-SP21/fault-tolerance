3e6f688e2200ed77f061766989233446487d66f4
Cody D'Ambrosio <cjd218@lehigh.edu>
2021-02-23 08:36:48 -0500

    Improve parsing JSON and setting backup info
    Update storing primary and backup data in Server class
    implement JSON parsing to actually store the data
    
    Signed-off-by: Cody D'Ambrosio <cjd218@lehigh.edu>

--- include/fault_tolerance.h
+++ include/fault_tolerance.h
@@ -14,19 +14,27 @@
  *
  */
 #include <string>
+#include <vector>
 #include "kvcg_logging.h"
 
 extern std::string CFG_FILE;
 
 // Base class
 class Node {
+protected:
+  std::string hostname;
 public:
   virtual int initialize() { return 0; }
+  void setName(std::string n) { hostname = n; }
+  std::string getName() { return hostname; }
 };
 
 class Server: public Node {
 private:
-  bool primary;
+  std::vector<std::pair<int, int>> primaryKeys; // primary key ranges
+
+  std::vector<Server*> backupServers; // servers backing up this ones primaries
+  std::vector<Server*> primaryServers; // servers whose keys this one is backing up
 public:
 
   /**
@@ -41,15 +49,71 @@ public:
   int initialize();
 
   /**
+   *
+   * Get vector of primary key ranges
+   *
+   * @param None
+   *
+   * @return vector of min/max key range pairs
+   *
+   */
+  std::vector<std::pair<int, int>> getPrimaryKeys() { return primaryKeys; }
+
+  /**
+   *
+   * Add key range to primary list
+   *
+   * @param keyRange - std::pair<int, int> of min and max key
+   *
+   * @return bool - true if added successfully, false otherwise
+   *
+   */
+  bool addKeyRange(std::pair<int, int> keyRange);
+
+  /**
+   *
+   * Add server who this one is backing up
+   *
+   * @param s - Server to add to primary server list
+   *
+   * @return bool - true if added successfully, false otherwise
+   *
+   */
+  bool addPrimaryServer(Server* s);
+
+  /**
+   *
+   * Add server who is backing this one up
+   *
+   * @param s - Server to add to backup server list
+   *
+   * @return bool - true if added successfully, false otherwise
+   *
+   */
+  bool addBackupServer(Server* s);
+
+  /**
    * 
    * Check if server is running as primary
+   * for a given key
    * 
    * @param None
    *
    * @return bool - true if primary, false otherwise
    *
    */
-  bool isPrimary() { return primary; }
+  bool isPrimary(int key);
+
+  /**
+   *
+   * Check if server is running as backup
+   * for a given key
+   *
+   * @param None
+   *
+   * @return bool - true if backup, false otherwise
+   *
+   */
 };
 
 class Client: public Node {
--- src/fault_tolerance.cc
+++ src/fault_tolerance.cc
@@ -6,6 +6,7 @@
 #include "fault_tolerance.h"
 #include <iostream>
 #include <sstream>
+#include <boost/asio/ip/host_name.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/foreach.hpp>
@@ -16,8 +17,11 @@ namespace pt = boost::property_tree;
 std::string CFG_FILE = "./kvcg.json";
 int LOG_LEVEL = INFO;
 
+const auto HOSTNAME = boost::asio::ip::host_name();
 
-int parse_json_file() {
+std::vector <Server*> foundServers;
+
+int parse_json_file(Server* inServer) {
     int status = 0;
     int backupcnt = 0;
     LOG(DEBUG) << "Opening file: " << CFG_FILE;
@@ -26,15 +30,56 @@ int parse_json_file() {
     try {
         pt::read_json(CFG_FILE, root);
         for (pt::ptree::value_type &server : root.get_child("servers")) {
-            LOG(DEBUG) << server.second.get<std::string>("name");
-            backupcnt = 0;
-            BOOST_FOREACH(pt::ptree::value_type &backup, server.second.get_child("backups")) {
-                LOG(DEBUG) << "  backup: " << backup.second.data();
-                backupcnt++;
+            std::string server_name = server.second.get<std::string>("name");
+            LOG(DEBUG3) << "Parsing server: " << server_name;
+            std::pair<int, int> keyRange = {server.second.get<int>("minKey"), server.second.get<int>("maxKey")};
+            LOG(DEBUG3) << "Key range: " << keyRange.first << "-" << keyRange.second;
+            Server* primServer = NULL;
+            for (auto& foundServer : foundServers) {
+                if (foundServer->getName() == server_name) {
+                    primServer = foundServer;
+                    break;
+                }
             }
-            if (backupcnt != 1) {
-                // TBD: For now only support exactly 1 backup
-                LOG(ERROR) << "Only supporting 1 backup";
+            if (primServer == NULL) {
+               primServer = new Server();
+               primServer->setName(server_name);
+               foundServers.push_back(primServer);
+            }
+            primServer->addKeyRange(keyRange);
+            BOOST_FOREACH(pt::ptree::value_type &backup, server.second.get_child("backups")) {
+                std::string backupName = backup.second.data();
+                // See if we have this server already
+                Server* backupServer = NULL;
+                for (auto& foundServer : foundServers) {
+                    if (foundServer->getName() == backupName) {
+                        backupServer = foundServer;
+                        break;
+                    }
+                }
+                if (backupServer == NULL) {
+                    backupServer = new Server();
+                    backupServer->setName(backupName);
+                    foundServers.push_back(backupServer);
+                }
+                LOG(DEBUG3) <<"Adding backup: " << backupServer->getName();
+                backupServer->addPrimaryServer(primServer);
+                primServer->addBackupServer(backupServer);
+            }
+        }
+
+        if (inServer) {
+            // assign to server in foundServer list
+            bool matched = false;
+            for (auto& foundServer : foundServers) {
+                if(foundServer->getName() == HOSTNAME) {
+                    LOG(DEBUG3) << "Matched config server to " << HOSTNAME;
+                    *inServer = *foundServer;
+                    matched = true;
+                }
+            }
+            if(!matched) {
+                LOG(ERROR) << "Could not find server in config file: " << HOSTNAME;
                 status = 1;
                 goto exit;
             }
@@ -52,19 +97,66 @@ exit:
 int Server::initialize() {
     int status = 0;
     LOG(INFO) << "Initializing Server";
+    LOG(INFO) << "Hostname: " << HOSTNAME;
+    this->setName(HOSTNAME);
 
-    if (status = parse_json_file())
+    if (status = parse_json_file(this))
         goto exit;
 
+    // Log this server configuration
+    LOG(INFO) << "Primary Keys:";
+    for (auto keyRange : primaryKeys) {
+        LOG(INFO) << "  [" << keyRange.first << ", " << keyRange.second << "]";
+    }
+    LOG(INFO) << "Backup Servers:";
+    for (auto backup : backupServers) {
+        LOG(INFO) << "  " << backup->getName();
+    }
+    LOG(INFO) << "Backing up primaries:";
+    for (auto primary : primaryServers) {
+        LOG(INFO) << "  " << primary->getName();
+        for (auto keyRange : primary->getPrimaryKeys()) {
+            LOG(DEBUG) << "    [" << keyRange.first << ", " << keyRange.second << "]";
+        }
+    }
 exit:
     return status;
+}
+
+bool Server::addKeyRange(std::pair<int, int> keyRange) {
+  // TODO: Validate input
+  primaryKeys.push_back(keyRange);
+  return true;
+}
+
+bool Server::addPrimaryServer(Server* s) {
+  // TODO: Validate input
+  primaryServers.push_back(s);
+  return true;
+}
+
+
+bool Server::addBackupServer(Server* s) {
+  // TODO: Validate input
+  backupServers.push_back(s);
+  return true;
+}
+
+
+bool Server::isPrimary(int key) {
+    for (auto el : primaryKeys) {
+        if (key >= el.first && key <= el.second) {
+            return true;
+        }
+    }
+    return false;
 }
 
 int Client::initialize() {
     int status = 0;
     LOG(INFO) << "Initializing Client";
 
-    if (status = parse_json_file())
+    if (status = parse_json_file(NULL))
         goto exit;
 
 exit:
