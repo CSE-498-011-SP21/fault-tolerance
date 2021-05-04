9ea46274fb0cbe166b31ad005e8f1efa69996dc9
Cody D'Ambrosio <61693810+cjd218@users.noreply.github.com>
2021-05-04 09:35:30 -0400

    Update server.cc

--- src/server.cc
+++ src/server.cc
@@ -824,7 +824,7 @@ int ft::Server::logRequest(std::vector<RequestWrapper<unsigned long long, data_t
                 skippedBitmask = 0;
                 try {
                   dataSize = serialize2(backup->logDataBuf.get()+2+offset, logBufSize-offset, req);
-                  if (offset + dataSize > logBufSize) {
+                  if (offset + 2 + dataSize > logBufSize) {
                     // serialize2 should've raise an exception, force it
                     throw std::overflow_error("MR buffer filled");
                   }
